ec7488100f029f35e26e4016b1a513effc91090f
Alexeev Konstantin <myworkmailbox@list.ru>
2019-01-14 21:30:45 +0500

    release 0.0.1

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+  
+  $('.custom-num-inc').on('click', function(){
+    var parent = $(this).parent();
+    var input = $(parent).find('input');
+    var value = $(input).val();
+
+    if(value == ''){
+      $(input).val(0);
+    }  
+    else if(!isNaN(parseInt(value))){
+      value =  parseInt(value);
+      value += 1;
+      $(input).val(value);
+    }  
+  });
+
+  $('.custom-num-dec').on('click', function(){
+    var parent = $(this).parent();
+    var input = $(parent).find('input');
+    var value = $(input).val();
+
+    if(value == ''){
+      $(input).val(0);
+    }  
+    else if(!isNaN(parseInt(value))){
+      value =  parseInt(value);
+      if(value > 0){
+        value -= 1;
+        $(input).val(value);
+      }
+    }  
+  });
+
+  $('.nav-pills > li').on('click', function(e){
+
+    e.preventDefault();
+
+    var navPillsItemNum = $(this).attr('data-nav-pills-item');
+    var tableWrapper = $('.block-wrapper[data-item-num="'+navPillsItemNum+'"]');
+
+    $('.nav-pills > li').removeClass('active');
+    $('.block-wrapper').removeClass('active');
+
+    $(this).addClass('active');
+    $(tableWrapper).addClass('active');
+  });
+});
